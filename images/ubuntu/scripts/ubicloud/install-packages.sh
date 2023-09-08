79d25f10c376e1ef9c514cbf0632552f98daf505
Enes Cakir <enes@cakir.web.tr>
2023-09-08 16:29:10 +0300

    [ubicloud] Make Ubuntu images Ubicloud compatible

---
+++ images/ubuntu/scripts/ubicloud/install-packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -xe
+
+source $HELPER_SCRIPTS/os.sh
+source $HELPER_SCRIPTS/install.sh
+
+apt-get update
+
+# sysstat is already installed, but it's not enabled by default
+apt-get install sysstat
+systemctl enable sysstat
+systemctl start sysstat
