ae781adb837f2d6a50defae5eeb08cbce3d9d19a
furkan sahin <sahinffurkan@gmail.com>
2024-05-22 13:28:22 +0200

    Add nvidia-cuda-toolkit to the GPU image

--- images/ubuntu/scripts/ubicloud/nvidia-driver.sh
+++ images/ubuntu/scripts/ubicloud/nvidia-driver.sh
@@ -6,11 +6,12 @@ set -euo pipefail
 KEYRING=cuda-keyring_1.1-1_all.deb
 ARCH=x86_64
 NV_DRIVER_VERSION=550
+CUDA_TOOLKIT_VERSION=12.5
 
 wget -q "https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/${ARCH}/${KEYRING}"
 sudo dpkg --install "${KEYRING}"
 sudo apt-get update
 
-sudo apt-get -y install "nvidia-driver-${NV_DRIVER_VERSION}"
+sudo apt-get -y install "nvidia-driver-${NV_DRIVER_VERSION}" "nvidia-cuda-toolkit-${CUDA_TOOLKIT_VERSION}"
 
 sudo dpkg --purge "$(dpkg -f "${KEYRING}" Package)"
