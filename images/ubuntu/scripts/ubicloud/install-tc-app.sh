6d4a93e5b9173339eecbdff1a00e76c20d62b753
Burak Velioglu <velioglub@ubicloud.com>
2024-09-16 01:13:37 +0300

    Fetch tcapp to the runner image

---
+++ images/ubuntu/scripts/ubicloud/install-tc-app.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+################################################################################
+##  File:  install-tc-app.sh
+##  Desc:  Download and Install tcapp
+################################################################################
+
+# Source the helpers for use with the script
+source $HELPER_SCRIPTS/os.sh
+source $HELPER_SCRIPTS/install.sh
+
+download_url=$(resolve_github_release_asset_url "velioglu/exec_repo" 'test("tcapp-linux-'"$(get_arch "x64" "arm64")"'-[0-9]+\\.[0-9]{3}\\.[0-9]+\\.tar\\.gz$")' "latest")
+archive_name="${download_url##*/}"
+archive_path=$(download_with_retry "$download_url")
+
+mkdir -p /usr/local/share/tcapp
+tar xzf "$archive_path" -C /usr/local/share/tcapp
+
+mkdir -p /opt/tcapp-cache
+mv "$archive_path" "/opt/tcapp-cache/$archive_name"
