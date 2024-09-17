41226bae2e84f8cd6ea238c71b6ec0bee072b238
Burak Velioglu <velioglub@ubicloud.com>
2024-09-17 13:45:36 +0300

    Run as service

--- images/ubuntu/scripts/ubicloud/install-tc-app.sh
+++ images/ubuntu/scripts/ubicloud/install-tc-app.sh
@@ -15,5 +15,22 @@ archive_path=$(download_with_retry "$download_url")
 mkdir -p /usr/local/share/tcapp
 tar xzf "$archive_path" -C /usr/local/share/tcapp
 
+cat <<EOF > /etc/systemd/system/tcapp.service
+[Unit]
+Description=Ubicloud Cache Proxy
+
+[Service]
+ExecStart=/bin/bash -c "/usr/local/share/tcapp/tcapp > ~/cacheproxy.log 2>&1"
+WorkingDirectory=/usr/local/share/tcapp
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl start tcapp.service
+systemctl enable tcapp.service
+
 mkdir -p /opt/tcapp-cache
 mv "$archive_path" "/opt/tcapp-cache/$archive_name"
