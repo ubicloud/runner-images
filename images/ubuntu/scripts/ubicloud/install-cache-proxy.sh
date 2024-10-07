de6f7480c185abcbe39f921a7f8c2c412aa50722
Burak Velioglu <velioglub@ubicloud.com>
2024-10-07 19:29:41 +0300

    [ubicloud] Download cache proxy and run it as service
    To support transparent cache proxy, downloading the binaries for
    cache proxy server and running it as a service.

---
+++ images/ubuntu/scripts/ubicloud/install-cache-proxy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+################################################################################
+##  File:  install-cache-proxy.sh
+##  Desc:  Download and Install cache proxy
+################################################################################
+# Source the helpers for use with the script
+source $HELPER_SCRIPTS/os.sh
+source $HELPER_SCRIPTS/install.sh
+
+if is_x64; then
+  download_url="https://pub-dd18084e939b4f87816c08c17faeb647.r2.dev/cache-proxy-linux-x64.tar.gz"
+elif is_arm64; then
+  download_url="https://pub-dd18084e939b4f87816c08c17faeb647.r2.dev/cache-proxy-linux-arm64.tar.gz"
+else
+  echo "Unsupported architecture"
+  exit 1
+fi
+archive_path=$(download_with_retry "$download_url")
+
+mkdir -p /usr/local/share/cache-proxy
+tar xzf "$archive_path" -C /usr/local/share/cache-proxy
+
+cat <<EOF > /etc/systemd/system/cache-proxy.service
+[Unit]
+Description=Ubicloud Cache Proxy
+[Service]
+ExecStart=/bin/bash -c "/usr/local/share/cache-proxy/cache-proxy > /var/log/cacheproxy.log 2>&1"
+WorkingDirectory=/usr/local/share/cache-proxy
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl start cache-proxy.service
+systemctl enable cache-proxy.service
