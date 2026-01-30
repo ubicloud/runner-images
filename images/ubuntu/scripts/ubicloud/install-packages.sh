9cf55aa3ebc034a66c61bc6486d9e0dd3bc6c295
Enes Cakir <enes@cakir.web.tr>
2026-01-30 19:28:54 +0300

    [ubicloud] Install ubi CLI

--- images/ubuntu/scripts/ubicloud/install-packages.sh
+++ images/ubuntu/scripts/ubicloud/install-packages.sh
@@ -9,3 +9,15 @@ apt-get update
 apt-get install sysstat
 systemctl enable sysstat
 systemctl start sysstat
+
+# Install ubi CLI
+if is_x64; then
+    download_url=$(resolve_github_release_asset_url "ubicloud/cli" "test(\"linux-amd64-.*.tar.gz$\")" "latest")
+elif is_arm64; then
+    download_url=$(resolve_github_release_asset_url "ubicloud/cli" "test(\"linux-arm64-.*.tar.gz$\")" "latest")
+else
+  echo "Unsupported architecture"
+  exit 1
+fi
+archive_path=$(download_with_retry "$download_url")
+tar xzf "$archive_path" -C /usr/local/bin ubi
