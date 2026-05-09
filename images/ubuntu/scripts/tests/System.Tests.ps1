018643b6db01034b6063cc1d0cd72b47f60f5f56
Enes Cakir <enes@cakir.web.tr>
2026-05-09 12:40:42 +0300

    [ubicloud] Revert read_ahead_kb config
    We don't use sd drives on Ubicloud.
    It causes issue on the AWS runner since they have different configs.

--- images/ubuntu/scripts/tests/System.Tests.ps1
+++ images/ubuntu/scripts/tests/System.Tests.ps1
@@ -25,23 +25,23 @@ Describe "fwupd removed" {
 
 # https://github.com/actions/runner-images/issues/13770
 # Linux kernel 6.17 changed read_ahead_kb from 128 to 4096 on Azure VMs, causing I/O thrashing
-Describe "ReadAhead udev rule" -Skip:(Test-IsUbuntu22) {
-    It "udev rule file exists" {
-        "/etc/udev/rules.d/99-readahead.rules" | Should -Exist
-    }
-
-    It "udev rule covers SCSI and NVMe devices with correct read_ahead_kb value" {
-        $content = Get-Content "/etc/udev/rules.d/99-readahead.rules" -Raw
-        $content | Should -Match 'KERNEL=="sd\*\|nvme\*n\*"'
-        $content | Should -Match 'ATTR\{queue/read_ahead_kb\}="128"'
-    }
-
-    It "All SCSI and NVMe devices have read_ahead_kb set to 128" {
-        $devices = Get-ChildItem "/sys/block/sd*/queue/read_ahead_kb", "/sys/block/nvme*n*/queue/read_ahead_kb" -ErrorAction SilentlyContinue
-        $devices | Should -Not -BeNullOrEmpty -Because "there should be at least one sd* or nvme* block device"
-        foreach ($dev in $devices) {
-            $value = (Get-Content $dev.FullName).Trim()
-            $value | Should -Be "128" -Because "read_ahead_kb for $($dev.FullName) should be 128 to prevent I/O thrashing"
-        }
-    }
-}
+# Describe "ReadAhead udev rule" -Skip:(Test-IsUbuntu22) {
+#     It "udev rule file exists" {
+#         "/etc/udev/rules.d/99-readahead.rules" | Should -Exist
+#     }
+#
+#     It "udev rule covers SCSI and NVMe devices with correct read_ahead_kb value" {
+#         $content = Get-Content "/etc/udev/rules.d/99-readahead.rules" -Raw
+#         $content | Should -Match 'KERNEL=="sd\*\|nvme\*n\*"'
+#         $content | Should -Match 'ATTR\{queue/read_ahead_kb\}="128"'
+#     }
+#
+#     It "All SCSI and NVMe devices have read_ahead_kb set to 128" {
+#         $devices = Get-ChildItem "/sys/block/sd*/queue/read_ahead_kb", "/sys/block/nvme*n*/queue/read_ahead_kb" -ErrorAction SilentlyContinue
+#         $devices | Should -Not -BeNullOrEmpty -Because "there should be at least one sd* or nvme* block device"
+#         foreach ($dev in $devices) {
+#             $value = (Get-Content $dev.FullName).Trim()
+            $value | Should -Be "128" -Because "read_ahead_kb for $($dev.FullName) should be 128 to prevent I/O#  thrashing"
+#         }
+#     }
+# }
